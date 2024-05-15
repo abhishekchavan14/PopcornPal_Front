a14ec8a8cb5342278589e4c2bd614b52e94ffc48
Abhishek Chavan <abhishekchavan940@gmail.com>
2024-05-15 20:28:53 +0530

    Adding profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,23 @@ import { IoMdMenu, IoMdClose } from "react-icons/io";
 import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks";
 import SearchField from "./SearchField";
+import { CgProfile } from "react-icons/cg";
+// import { getReviewsByOwner } from "../api/review";
 
 export default function Navbar() {
   const location = useLocation(); //used in keeping the Home,About etc in Navbar active when we on that corresponding path
 
   //to toggle between the menu and close icon (for mobile devices)
   const [nav, setNav] = useState(false);
+  const [displayProfile, setDisplayProfile] = useState(false);
   const { authInfo, handleLogout } = useAuth();
   const { isLoggedIn } = authInfo;
   function handleMenuClick() {
     setNav(!nav);
   }
-
+  const handleProfileClick = () => {
+    setDisplayProfile((prev) => !prev);
+  };
   return (
     <div className="bg-primary p-4 md:pl-10 shadow-md shadow-black">
       <div className="flex items-center justify-between">
@@ -63,9 +68,9 @@ export default function Navbar() {
               </Link>
             )}
           </li>
-          <li className="mr-8 z-10">
+          <li className=" z-10">
             {isLoggedIn ? (
-              <div className="flex space-x-4">
+              <div className="flex items-center space-x-4">
                 <SearchField />
                 <button
                   onClick={handleLogout}
@@ -77,6 +82,11 @@ export default function Navbar() {
                 >
                   Log Out
                 </button>
+                <CgProfile
+                  className="text-3xl text-white hover:text-primary-red duration-300 cursor-pointer"
+                  onClick={handleProfileClick}
+                />
+                <Profile visible={displayProfile} />
               </div>
             ) : (
               <Link
@@ -141,3 +151,39 @@ export default function Navbar() {
     </div>
   );
 }
+
+const Profile = async ({ visible }) => {
+  const { authInfo, handleLogout } = useAuth();
+  const { profile } = authInfo;
+  // await getReviewsByOwner(profile.id);
+  {
+    return visible ? (
+      <>
+        <div className="w-[20%] h-screen bg-dark-grey flex flex-col space-y-5 py-16 items-center absolute top-20 shadow-xl shadow-black right-0 text-white duration-500">
+          <CgProfile className="text-8xl text-dark-subtle" />
+          <div className="flex flex-col items-center">
+            <p className="text-3xl">{profile.username}</p>
+            <p className="text-sm italic">{profile.email}</p>
+          </div>
+          <div>Reviewed 100 movies</div>
+          <div className="border py-2 px-4">
+            <button
+              onClick={handleLogout}
+              className={
+                location.pathname === "/"
+                  ? "text-primary-red"
+                  : "text-white hover:text-primary-red duration-300 cursor-pointer"
+              }
+            >
+              Log Out
+            </button>
+          </div>
+        </div>
+      </>
+    ) : (
+      <>
+        <div className="w-[20%] h-screen bg-dark-grey flex flex-col justify-center items-center absolute top-20 right-[-100%] text-white duration-500"></div>
+      </>
+    );
+  }
+};
